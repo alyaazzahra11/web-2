31eee1dc877779cc267652fd5d847775f71a62b4
alyaazzahra11 <alyaazzahra113125@gmail.com>
2025-02-22 19:39:59 +0700

    Add files via upload

---
+++ variabel_user.php
@@ -0,0 +1,10 @@
+<?php
+// definisikan variables
+$nama = 'Alya Az-Zahra';
+$umur = 19;
+$berat = 50;
+echo 'Nama : ' . $nama;
+echo '<br/>Umur : ' . $umur.' Tahun';
+echo '<br/>Berat : '.$berat.' Kg';
+echo "<br/>Hello $nama Apa kabar?";
+?>
